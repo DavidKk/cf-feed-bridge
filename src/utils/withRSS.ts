91928cbadda11dda15ea6b2a4ac2ab2ac1251f38
DavidJones <qowera@gmail.com>
2024-07-11 00:20:42 +0800

    feat: add cache

--- src/utils/withRSS.ts
+++ src/utils/withRSS.ts
@@ -2,14 +2,41 @@ import type { IRequest } from 'itty-router'
 import { XMLParser } from 'fast-xml-parser'
 import { RSSHeaders, FeedHeaders } from '../constants/header'
 
-export function withRSS(xmlDocHandler: (xmlDoc: any) => Promise<Record<string, any>>) {
-  return async function handler(request: IRequest) {
-    const { url } = request?.query || {}
+export interface WithRSSOptions {
+  // default 60s
+  cacheTtl?: number
+}
+
+export function withRSS(xmlDocHandler: (xmlDoc: any) => Promise<Record<string, any>>, options?: WithRSSOptions) {
+  return async function handler(req: IRequest, _env: Env, ctx: ExecutionContext) {
+    const { cacheTtl = 60 } = options || {}
+    const { url } = req?.query || {}
     if (typeof url !== 'string') {
       throw new Error('url is required')
     }
 
-    const response = await fetch(url, {
+    const cacheKey = new Request(url, req)
+    const cacheKeyString = cacheKey.url
+    const cache = caches.default
+
+    console.log(`Checking cache for key: ${cacheKeyString}`)
+    let response = await cache.match(cacheKey)
+    if (response) {
+      console.log(`Cache hit for key: ${cacheKeyString}`)
+
+      const clonedResponse = response.clone()
+      const newHeaders = new Headers(clonedResponse.headers)
+      newHeaders.append('hit-cache', cacheKeyString)
+
+      return new Response(clonedResponse.body, {
+        status: clonedResponse.status,
+        statusText: clonedResponse.statusText,
+        headers: newHeaders,
+      })
+    }
+
+    console.log(`Cache miss for key: ${cacheKeyString}`)
+    response = await fetch(url, {
       method: 'GET',
       headers: RSSHeaders,
     })
@@ -24,8 +51,15 @@ export function withRSS(xmlDocHandler: (xmlDoc: any) => Promise<Record<string, a
     const json = await xmlDocHandler(xmlDoc)
     const jsonText = JSON.stringify(json)
 
-    return new Response(jsonText, {
-      headers: FeedHeaders,
+    const responseToCache = new Response(jsonText, {
+      headers: {
+        ...FeedHeaders,
+        'Cache-Control': `s-maxage=${cacheTtl}`,
+      },
     })
+
+    console.log(`Caching response for key: ${cacheKeyString}`)
+    ctx.waitUntil(cache.put(cacheKey, responseToCache.clone()))
+    return responseToCache
   }
 }
